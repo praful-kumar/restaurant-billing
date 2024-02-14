5b66552e57bf3f9168df20b059f7bfb56c7812fa
praful <praful321997@gmail.com>
2024-02-14 21:41:56 +0530

    test

---
+++ src/test/java/com/restaurant/restaurantbilling/RestaurantBillingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.restaurant.restaurantbilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("");
+	}
+
+}
