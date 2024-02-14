a00ed3b138e0f45bc0aa1d8613b8b60bfd7947be
praful <praful321997@gmail.com>
2024-02-14 21:58:34 +0530

    Remove ignored files

---
+++ src/main/java/com/restaurant/restaurantbilling/RestaurantBillingApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.restaurantbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantBillingApplication.class, args);
+	}
+
+}
